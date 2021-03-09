570ff08a78a1cb6d6dd5a45ccff74c193f08dd59
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-09 21:39:16 +0100

    Styled comp. update

--- src/components/AccountBalance/AccountBalance.jsx
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components'
 
 const Section = styled.section`
-  border: 1px solid red;
+  font-size: 2rem;
+  text-align: left;
+  padding: 1.5rem 0 1.5rem 5rem;
 `;
 
 export default class AccountBalance extends Component {
   render() {
     return (
-      <section>
-        $ {this.props.amount}
-      </section>
+      <Section>
+        Balance: ${this.props.amount}
+      </Section>
     );
   }
 }
